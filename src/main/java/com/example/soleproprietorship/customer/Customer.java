3259d95c78b87b1fccca71d0a78df1880e4c031f
Karol Zięba <kz516749@gmail.com>
2023-04-26 18:13:47 +0200

    Encje

--- src/main/java/com/example/soleproprietorship/customer/Customer.java
+++ src/main/java/com/example/soleproprietorship/customer/Customer.java
@@ -1,6 +1,7 @@
 package com.example.soleproprietorship.customer;
 
 import com.example.soleproprietorship.transaction.Transaction;
+import com.example.soleproprietorship.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +18,7 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idCustomer")
     private long idCustomer;
+
     @Column(name = "name")
     private String name;
 
@@ -26,26 +28,20 @@ public class Customer {
     @Column(name = "address")
     private String address;
 
-    @Column(name = "phoneNumber", length = 10)
+    @Column(name = "phoneNumber", length = 9)
     private String phoneNumber;
 
     @Column(name = "email")
     @Email
     private String email;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idUser")
+    private User user;
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer")
     private List<Transaction> transactions;
 
-
     public Customer() {
-
-    }
-
-    public Customer(String name, String surName, String address, String phoneNumber, String email) {
-        this.name = name;
-        this.surName = surName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
     }
 }
--- src/main/java/com/example/soleproprietorship/transaction/Transaction.java
+++ src/main/java/com/example/soleproprietorship/transaction/Transaction.java
@@ -1,45 +1,71 @@
 package com.example.soleproprietorship.transaction;
 
 import com.example.soleproprietorship.customer.Customer;
+import com.example.soleproprietorship.product.Product;
+import com.example.soleproprietorship.service.Service;
+import com.example.soleproprietorship.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-@Entity(name = "Transactions")
+@Entity
+@Table(name = "Transactions")
 @Getter
 @Setter
 public class Transaction {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idTransaction")
     private long idTransaction;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "idCustomer")
-    private Customer customer;
+    @Column(name = "date")
+    private LocalDateTime date;
 
-    @Column(name = "dateOfTransaction")
-    private LocalDateTime dateOfTransaction;
-
-    @Column(name = "transactionAmount")
-    private Double transactionAmount;
+    @Column(name = "price")
+    private Double price;
 
     @Column(name = "description")
     private String description;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idUser")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idCustomer")
+    private Customer customer;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "TransactionProducts",
+            joinColumns = {
+                @JoinColumn(name = "idTransaction")
+            },
+            inverseJoinColumns = {
+                @JoinColumn(name = "idProduct")
+            }
+    )
+    private List<Product> products;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "TransactionServices",
+            joinColumns = {
+                    @JoinColumn(name = "idTransaction")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "idService")
+            }
+    )
+    private List<Service> services;
+
     public Transaction() {
-
     }
-
-    public Transaction(Customer customer, Double transactionAmount, String description) {
-        this.customer = customer;
-        this.dateOfTransaction = LocalDateTime.now();
-        this.transactionAmount = transactionAmount;
-        this.description = description;
-    }
-
-
 }
